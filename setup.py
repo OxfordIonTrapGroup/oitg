4f72cd9bc0fdd782c2fe4e90f3f60fdcbc8606b9
Chris Ballance <chris.ballance@physics.ox.ac.uk>
2017-02-11 17:56:07 +0000

    setup: use find_packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='oitg',
-      version='0.1',
-      packages=['oitg']
-      )
+setup(name = 'oitg',
+      version = '0.1',
+      packages = find_packages(),
+    )
