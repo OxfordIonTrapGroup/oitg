ad37e968cf3b24261fdc27039d62e0450c99d95d
Tim Ballance <timothy.ballance@physics.ox.ac.uk>
2017-04-10 09:03:40 +0100

    Added right-side fit function for half_lorentzian

--- oitg/fitting/half_lorentzian.py
+++ oitg/fitting/half_lorentzian.py
@@ -36,6 +36,18 @@ def fitting_function_left(x, p):
 
     return y
 
+def fitting_function_right(x, p):
+
+    y = p['a']*(0.5*p['fwhm'])**2
+    y /= (x-p['x0'])**2 + (0.5*p['fwhm'])**2
+    y += p['y0']
+    
+    y = np.where(x > p['x0'], y, p['y0'])
+
+    return y
 
 half_lorentzian_left = FitBase.FitBase(['x0', 'y0', 'a', 'fwhm'], fitting_function_left,
+                       parameter_initialiser=parameter_initialiser)
+
+half_lorentzian_right = FitBase.FitBase(['x0', 'y0', 'a', 'fwhm'], fitting_function_right,
                        parameter_initialiser=parameter_initialiser)
