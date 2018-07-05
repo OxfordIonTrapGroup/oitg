6bca5d5b113a61f19dca7529a78f780e8a4f0660
Chris Ballance <chris.ballance@physics.ox.ac.uk>
2018-07-05 23:34:43 +0100

    setup: require h5py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name = 'oitg',
-      version = '0.1',
-      packages = find_packages(),
-    )
+setup(
+    name = 'oitg',
+    version = '0.1',
+    packages = find_packages(),
+    install_requires=["numpy", "h5py"],
+)
