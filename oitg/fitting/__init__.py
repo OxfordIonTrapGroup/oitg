e69defb975fd311c1704761342649f9ebdce2c46
David Nadlinger <code@klickverbot.at>
2018-07-15 19:17:50 +0100

    fitting: Import all fit type definitions in package __init__

--- oitg/fitting/__init__.py
+++ oitg/fitting/__init__.py
@@ -1,3 +1,14 @@
-
-
-
+from .cos import cos, cos_fft
+from .cos_2 import cos_2, cos_2_fft
+from .gaussian import gaussian
+from .gaussian_beam import gaussian_beam
+from .half_lorentzian import half_lorentzian_left, half_lorentzian_right
+#from line import line    # Seems unfinished.
+from .lorentzian import lorentzian
+from .parabola import parabola
+from .rabi_flop import rabi_flop
+from .sin import sin, sin_fft
+from .sin_2 import sin_2, sin_2_fft
+from .sinc_2 import sinc_2
+from .step_function import step_function
+from .tanh import tanh
