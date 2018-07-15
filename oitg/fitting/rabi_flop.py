25c0b34c2a032736e464f1d606d1533a690a4b1d
David Nadlinger <code@klickverbot.at>
2018-07-15 23:49:42 +0100

    fitting.rabi_flop: Improve initial guess heuristics

--- oitg/fitting/rabi_flop.py
+++ oitg/fitting/rabi_flop.py
@@ -7,25 +7,36 @@ from . import FitBase
 initial dead time to account for AOM/... switching effects.
 
 Currently only supports positive-sign scans (starting at y=1, not y=0).
+
+For guessing the initial parameters, it is assumed that the scan range contains
+the first minimum (i.e., the pi time), and is about 1/10 to 10 times in length.
 """
 
 def parameter_initialiser(x, y, p):
-    x_range = np.amax(x) - np.amin(x)
-    if x_range == 0.0:
-        x_range = 1.0
+    t_min = np.amin(x)
+    t_range = np.amax(x) - t_min
+    if t_range == 0.0:
+        t_range = 1.0
 
-    # Estimate frequency (using Long-Scargle periodogram, to support irregularly
-    # spaced x points).
+    # Estimate frequency. Starting with a Lomb-Scargle periodogram (which
+    # supports irregularly-spaced samples), we pick the strongest frequency
+    # component which leads to a pi time larger than t_min.
+    #
     # TODO: Could use better heuristics for frequency range based on minimum
     # distance between points -> aliasing.
-    freq = 2 * np.pi / x_range
+    freq = np.pi / t_range
     freqs = np.linspace(0.1 * freq, 10 * freq, 2 * len(x))
     pgram = lombscargle(x, y, freqs, precenter=True)
-    p["t_period"] = 2 * np.pi / freqs[np.argmax(pgram)]
+    freq_order = np.argsort(-pgram)
+    for f in freqs[freq_order]:
+        t = 2 * np.pi / f
+        if t / 2 > t_min:
+            p["t_period"] = t
+            break
 
     p["t_dead"] = 0.0
 
-    p["y_lower"] = 2 * np.mean(y) - 1
+    p["y_lower"] = np.clip(2 * np.mean(y) - 1, 0, 1)
 
     # TODO: Estimate decay time constant using RMS amplitude from global mean
     # in first and last chunk.
