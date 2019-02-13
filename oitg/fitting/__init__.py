933ba037f6354f4b2652592c44f9d74a3be29b30
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2019-02-13 10:54:13 +0000

    Revert previous 2 commits as likely unecessary
    If rabi_flop does not fit usually this is because the data are bad

--- oitg/fitting/__init__.py
+++ oitg/fitting/__init__.py
@@ -8,7 +8,6 @@ from .half_lorentzian import half_lorentzian_left, half_lorentzian_right
 from .lorentzian import lorentzian
 from .parabola import parabola, shifted_parabola
 from .rabi_flop import rabi_flop
-from .rabi_flop_no_decay import rabi_flop_no_decay
 from .sin import sin, sin_fft
 from .sin_2 import sin_2, sin_2_fft
 from .sinc_2 import sinc_2
