48853b99ecff5b029d06cf25309c9f9f5798cc86
Clemens Loschnauer <clemens.loschnauer@physics.ox.ac.uk>
2019-08-12 15:12:00 +0100

    fitting/sinusoid: expanded docstring and added to top-level module

--- oitg/fitting/sinusoid.py
+++ oitg/fitting/sinusoid.py
@@ -5,24 +5,41 @@ from .FitBase import FitBase
 
 
 def fitting_function(t, p_dict):
-    """sinusoid with dead time"""
-    y = p_dict['a'] * np.sin((t - p_dict['t_dead']) * p_dict['omega']
-                             + p_dict['phi']) \
-        + p_dict['c']
-    # hold constant during dead time
-    return np.where(t > p_dict['t_dead'], y,
-                    p_dict['c'] + p_dict['a'] * np.sin(p_dict['phi'])
-                    )
+    """
+    Sinusoid with dead time
+
+    This allows fitting of a function that is constant for t <= t_dead and
+    sinusoidal for t > t_dead, with a phase such that the transition at
+    t=t_dead is continuous.
+    A discontinuous transition from a constant level to a sinusoid is not
+    supported at the moment.
+
+    If the dead time is non-zero, the parameter "phi" refers to the phase
+    offset using a reference time of t=t_dead. This means that, for example,
+    if t_dead is non-zero and phi=0.0, the result is a sine curve that goes
+    through zero at t=t_dead, but would not necessarily do so at t=0
+    (extrapolating back through dead time).
+
+    If t_dead is known to be zero, best fit results are obtained if this
+    parameter is constrained manually when fitting.
+    """
+    y = p_dict["a"] * np.sin((t - p_dict["t_dead"]) * p_dict["omega"]
+                             + p_dict["phi"]) \
+        + p_dict["c"]
+    # Hold constant during dead time
+    return np.where(t > p_dict["t_dead"], y,
+                    p_dict["a"] * np.sin(p_dict["phi"]) + p_dict["c"])
 
 
-def init_all(t, y, p_dict):
-    """Initialise parameters under the general assumptions that:
-    t & y are 1D arrays of real numbers
-    associated data coordinates share the same index in t & y
+def parameter_initialiser(t, y, p_dict):
+    """
+    Initialise parameters under the following assumptions:
+    - t, y are 1D arrays of real numbers
+    - associated data coordinates share the same index in t and y
 
-    Note: Data may be in any order and arbitrarily spaced"""
-
-    # sort data by time (x-axis)
+    Note: Data may be in any order and arbitrarily spaced
+    """
+    # Sort data by time (x-axis)
     mask = np.argsort(t)
     t = t[mask]
     y = y[mask]
@@ -35,56 +52,64 @@ def init_all(t, y, p_dict):
     omega_list = 2 * np.pi * np.linspace(f_min, f_max, int(f_max / f_min))
     pgram = lombscargle(t, y, omega_list, precenter=True)
 
-    p_dict['omega'] = omega_list[np.argmax(pgram)]
-    p_dict['c'] = np.mean(y)
-    p_dict['a'] = np.sqrt(np.max(pgram) * 4 / len(y))
-    p_dict['t_dead'] = np.min(t)
+    p_dict["omega"] = omega_list[np.argmax(pgram)]
+    p_dict["c"] = np.mean(y)
+    p_dict["a"] = np.sqrt(np.max(pgram) * 4 / len(y))
+    p_dict["t_dead"] = np.min(t)
 
-    y0_norm = (y[0] - p_dict['c']) / p_dict['a']
+    y0_norm = (y[0] - p_dict["c"]) / p_dict["a"]
     if y0_norm <= -1.0:
-        p_dict['phi'] = -np.pi / 2
+        p_dict["phi"] = -np.pi / 2
     elif y0_norm >= 1.0:
-        p_dict['phi'] = np.pi / 2
+        p_dict["phi"] = np.pi / 2
     else:
-        p_dict['phi'] = np.arcsin(y0_norm)
-    return p_dict  # input is changed due to mutability of dict!
+        p_dict["phi"] = np.arcsin(y0_norm)
+    return p_dict
 
 
 def derived_params(p_dict, p_error_dict):
-    """calculate commonly used derived parameters and their error
+    """
+    Calculate commonly used derived parameters and their error
 
-    This method neglects parameter covariances!"""
-    p_dict['max'] = p_dict['c'] + np.abs(p_dict['a'])
-    p_dict['min'] = p_dict['c'] - np.abs(p_dict['a'])
-    p_dict['t_pi'] = p_dict['t_dead'] + np.pi / p_dict['omega']
-    p_dict['t_pi/2'] = p_dict['t_dead'] + np.pi / 2 / p_dict['omega']
-    p_dict['period'] = 2 * np.pi / p_dict['omega']
+    This method neglects parameter covariances!
+    """
+    p_dict["max"] = p_dict["c"] + np.abs(p_dict["a"])
+    p_dict["min"] = p_dict["c"] - np.abs(p_dict["a"])
+    p_dict["t_pi"] = p_dict["t_dead"] + np.pi / p_dict["omega"]
+    p_dict["t_pi/2"] = p_dict["t_dead"] + np.pi / 2 / p_dict["omega"]
+    p_dict["period"] = 2 * np.pi / p_dict["omega"]
 
     # this error calculation neglects parameter covariance!
     # may want to upgrade FitBase to make covariance matrix available.
 
-    p_error_dict['max'] = np.sqrt(p_error_dict['c']**2 +
-                                  p_error_dict['a']**2)
-    p_error_dict['min'] = np.sqrt(p_error_dict['c']**2 +
-                                  p_error_dict['a']**2)
-    p_error_dict['t_pi'] = np.sqrt(p_error_dict['t_dead']**2 +
-                                   (np.pi / p_dict['omega'] *
-                                    (p_error_dict['omega'] / p_dict['omega'])
-                                    )**2
-                                   )
-    p_error_dict['t_pi/2'] = np.sqrt(p_error_dict['t_dead']**2 +
-                                     (np.pi / 2 / p_dict['omega'] *
-                                      (p_error_dict['omega'] / p_dict['omega'])
-                                      )**2
-                                     )
-    p_error_dict['period'] = 2 * np.pi / p_dict['omega'] * \
-                             (p_error_dict['omega'] / p_dict['omega'])
+    p_error_dict["max"] = \
+        np.sqrt(p_error_dict["c"]**2
+                + p_error_dict["a"]**2)
+
+    p_error_dict["min"] = \
+        np.sqrt(p_error_dict["c"]**2
+                + p_error_dict["a"]**2)
+
+    p_error_dict["t_pi"] = \
+        np.sqrt(p_error_dict["t_dead"]**2
+                + (np.pi / p_dict["omega"]
+                    * (p_error_dict["omega"] / p_dict["omega"]))**2)
+
+    p_error_dict["t_pi/2"] = \
+        np.sqrt(p_error_dict["t_dead"]**2
+                + (np.pi / 2 / p_dict["omega"]
+                    * (p_error_dict["omega"] / p_dict["omega"]))**2)
+
+    p_error_dict["period"] = \
+        2 * np.pi / p_dict["omega"] * (p_error_dict["omega"] / p_dict["omega"])
+
     return (p_dict, p_error_dict)
 
 
 sinusoid = FitBase(
     ["omega", "t_dead", "a", "c", "phi"],
-    fitting_function=fitting_function, parameter_initialiser=init_all,
+    fitting_function=fitting_function,
+    parameter_initialiser=parameter_initialiser,
     derived_parameter_function=derived_params,
     parameter_bounds={"omega": (0, np.inf),
                       "t_dead": (0, np.inf),
@@ -131,8 +156,8 @@ if __name__ == "__main__":
 
         plt.figure()
         plt.errorbar(t, y,
-                     yerr=np.ones(y.shape) * np.sqrt(1/3 - 1/4) * \
-                          amp * rel_noise,
+                     yerr=np.ones(y.shape) * np.sqrt(1/3 - 1/4)
+                          * amp * rel_noise,
                      ecolor='k',
                      label="input")
         plt.plot(x_fit, y_fit, color="y", label="fit")
