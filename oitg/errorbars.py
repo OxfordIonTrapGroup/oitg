errorbars: Fix factor of two in confidence interval size
I can't figure out what the original intent behind this was. From the
statsmodels Git history, `alpha` always had the current meaning.

Tested against statsmodels commit 4565348ae1cf0bd54879cb861888c2e762c8239e,
where it now returns error bars that are (in the large-N limit) equal to
sqrt(p * (1 - p) / (N - 1)).

Should probably add some unit tests as well.

oitg/errorbars.py
@@ -7,12 +7,12 @@ def binom_twosided(k, N):
     """Returns the estimated source probability and confidence interval from
     a sample of k Trues out of N samples.
     """
-    # Method 'beta' uses Clopper-Pearson method
-    # alpha (1 - exp(-1)) gives us the 68% confidence interval
     if k > N or k < 0:
         raise ValueError("k must be between 0 and N (k={}, N={})".format(k,N))
 
-    confint = proportion_confint(k, N, alpha=(1-np.exp(-1)), method='beta')
+    # 'beta' is Clopper–Pearson method; chosen alpha corresponds to 1σ of a normal
+    # distribution (68%).
+    confint = proportion_confint(k, N, alpha=0.3173, method='beta')
 
     # Strip out NaNs for confidence intervals at the boundary
     if np.isnan(confint[0]):