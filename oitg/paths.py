9b4fbdd8289a668ef506803a4f75016692435b57
David Nadlinger <code@klickverbot.at>
2019-12-11 23:13:54 +0000

    paths: Add type annotations

--- oitg/paths.py
+++ oitg/paths.py
@@ -5,17 +5,17 @@ computer/network setup.
 
 import os
 from datetime import date
+from typing import Optional
 
 
-def _get_user():
+def _get_user() -> str:
     try:
-        user = os.environ["OITG_USER"]
+        return os.environ["OITG_USER"]
     except KeyError:
         raise Exception("No user supplied, and no OITG_USER environment key")
-    return user
 
 
-def shared_area_path():
+def shared_area_path() -> str:
     r"""Return the standard path to the shared area on the current platform.
 
     For Windows, the standard mount point is ``Z:\``, and for Unix-like systems
@@ -29,7 +29,7 @@ def shared_area_path():
     raise Exception("Unknown OS")
 
 
-def analysis_root_path(user=None):
+def analysis_root_path(user: Optional[str] = None) -> str:
     """Return the path to the given users analysis directory on the shared area
     (``<shared_area>/Users/<user>/analysis``).
 
@@ -41,7 +41,7 @@ def analysis_root_path(user=None):
     return os.path.join(shared_area_path(), "Users", user, "analysis")
 
 
-def todays_analysis_path(day=None, user=None):
+def todays_analysis_path(day: Optional[str] = None, user: Optional[str] = None) -> str:
     """Return the path to the analysis directory for the given day, defaulting to today.
 
     The analysis directory is intended to be used as working space for analysing data
@@ -58,16 +58,16 @@ def todays_analysis_path(day=None, user=None):
         day = date.today().isoformat()
     if user is None:
         user = _get_user()
-
     path = os.path.join(analysis_root_path(user=user), day)
 
     if not os.access(path, os.R_OK):
         # If the dir does not exist, create it
         os.mkdir(path)
+
     return path
 
 
-def artiq_results_path(experiment=None):
+def artiq_results_path(experiment: Optional[str] = None) -> str:
     """Return the path to an experiment's ARTIQ results directory.
 
     The standard results path is ``<shared_area>/artiqResults/<experiment>``.
@@ -85,6 +85,5 @@ def artiq_results_path(experiment=None):
         except KeyError:
             raise Exception(
                 "No experiment supplied, and no OITG_EXPERIMENT environment key")
-    path = os.path.join(path, experiment)
 
-    return path
+    return os.path.join(path, experiment)
