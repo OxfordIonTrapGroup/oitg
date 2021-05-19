26e047ee3c1ed8d85f72f9f2fe33d55e4428ac52
David Nadlinger <code@klickverbot.at>
2021-05-19 22:36:55 +0100

    test_threshold: YAPF-ify to unbreak CI

--- test/test_threshold.py
+++ test/test_threshold.py
@@ -9,12 +9,17 @@ class ThresholdTest(unittest.TestCase):
         p_bright = 0.5
         dark_to_bright_rate = 1 / 1.168
         t_bin, threshold, p_error = optimise_readout(
-            bright_rate, dark_rate, dark_to_bright_rate=dark_to_bright_rate,
-            p_bright=p_bright,)
-        self.assertAlmostEqual(0.0010438517809917116, t_bin,
+            bright_rate,
+            dark_rate,
+            dark_to_bright_rate=dark_to_bright_rate,
+            p_bright=p_bright,
+        )
+        self.assertAlmostEqual(0.0010438517809917116,
+                               t_bin,
                                delta=0.01 * 0.0010438517809917116)
         self.assertEqual(58, threshold)
-        self.assertAlmostEqual(0.000285363868745659, p_error,
+        self.assertAlmostEqual(0.000285363868745659,
+                               p_error,
                                delta=0.01 * 0.000285363868745659)
 
     def test_optimise_t_bin(self):
@@ -23,11 +28,17 @@ class ThresholdTest(unittest.TestCase):
         p_bright = 0.5
         dark_to_bright_rate = 1 / 1.168
         t_bin, p_error = optimise_t_bin(
-            bright_rate, dark_rate, 58,
-            dark_to_bright_rate=dark_to_bright_rate, p_bright=p_bright,)
-        self.assertAlmostEqual(0.0010438517809917116, t_bin,
+            bright_rate,
+            dark_rate,
+            58,
+            dark_to_bright_rate=dark_to_bright_rate,
+            p_bright=p_bright,
+        )
+        self.assertAlmostEqual(0.0010438517809917116,
+                               t_bin,
                                delta=0.01 * 0.0010438517809917116)
-        self.assertAlmostEqual(0.000285363868745659, p_error,
+        self.assertAlmostEqual(0.000285363868745659,
+                               p_error,
                                delta=0.01 * 0.000285363868745659)
 
 
